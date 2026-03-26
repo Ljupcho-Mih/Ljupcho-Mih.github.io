d54065084e3e1d93c2e1f86e2e0397cae5c3565b
Ljupcho-Mih <lmihailovski@googlemail.com>
2026-03-26 17:44:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-const menuIcon=document.querySelector('.mobile-menu-icon'); const navMenu=document.querySelector('.nav-menu'); menuIcon.addEventListener('click',()=>{navMenu.classList.toggle('open');});
+const menu=document.querySelector('.mobile-menu-icon'); const nav=document.querySelector('.nav-menu'); menu.addEventListener('click',()=>{nav.classList.toggle('open');});

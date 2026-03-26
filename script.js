14ddc694a1e514d63435816d21b7145a044c4cc4
Ljupcho-Mih <lmihailovski@googlemail.com>
2026-03-26 10:04:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.querySelector('.mobile-menu-icon').onclick = ()=>{
  const menu = document.querySelector('.nav-menu');
  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
@@ -9,6 +8,6 @@ document.querySelectorAll('.accordion-header').forEach(h => {
  const b = h.nextElementSibling;
  const open = b.style.display === 'block';
  document.querySelectorAll('.accordion-body').forEach(x => x.style.display='none');
- b.style.display = open ? 'none' : 'block';
+ b.style.display = open ? 'none':'block';
  };
 });

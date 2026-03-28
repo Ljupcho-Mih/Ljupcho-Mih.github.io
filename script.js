21936a79235ceecdfe5eb27c41cca8f29fa5dbc4
Ljupcho-Mih <lmihailovski@googlemail.com>
2026-03-28 20:15:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const toggle=document.getElementById("menuToggle");
-const menu=document.getElementById("mobileMenu");
+
+
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("mobileMenu");
+
 toggle.addEventListener("click",()=>{
-if(menu.style.display==="flex"){menu.style.display="none";}
-else{menu.style.display="flex";}
+
+if(menu.style.display==="flex"){
+menu.style.display="none";
+}else{
+menu.style.display="flex";
+}
+
 });
-const headers=document.querySelectorAll(".accordion-header");
-headers.forEach(header=>{
-header.addEventListener("click",()=>{
-const body=header.nextElementSibling;
-if(body.style.display==="block"){body.style.display="none";}
-else{body.style.display="block";}
-});
-});
+

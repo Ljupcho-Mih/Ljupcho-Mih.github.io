10cd6a8bbce66e2bd9ac08b44a6dfed1b5afe3ef
Ljupcho-Mih <lmihailovski@googlemail.com>
2026-03-28 22:37:44 +0100

    Update script_final.js

--- script_final.js
+++ script_final.js
@@ -7,7 +7,4 @@ document.querySelectorAll('.accordion-header').forEach(header => {
     const body = header.nextElementSibling;
     const isOpen = body.style.display === 'block';
     document.querySelectorAll('.accordion-body')
-      .forEach(b => b.style.display = 'none');
-    body.style.display = isOpen ? 'none' : 'block';
-  });
-});
+      .

39b5ca9aee2970fd0454ab5c0ac7ec5787489c95
Ljupcho-Mih <lmihailovski@googlemail.com>
2026-03-29 21:07:12 +0200

    Update script_final.js

--- script_final.js
+++ script_final.js
@@ -1,7 +1,31 @@
+// MOBILE MENU
 document.getElementById('menuToggle').addEventListener('click', () => {
     document.getElementById('mobileMenu').classList.toggle('show');
 });
 
+// SMOOTH SCROLL WITHOUT # IN URL
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function(e) {
+
+        const href = this.getAttribute('href');
+
+        if (href.startsWith("#")) {
+            e.preventDefault();
+
+            const target = document.querySelector(href);
+
+            window.scrollTo({
+                top: target.offsetTop - 80,
+                behavior: "smooth"
+            });
+
+            // REMOVE HASH FROM URL
+            history.replaceState(null, null, ' ');
+        }
+    });
+});
+
+// ACCORDION
 document.querySelectorAll('.accordion-header').forEach(header => {
     header.addEventListener('click', () => {
         const body = header.nextElementSibling;
